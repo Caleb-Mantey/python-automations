bc1bc5db45dbf503d69a8737e7073bb4a7bfc6b2
Caleb Mantey <manteycaleb@gmail.com>
2025-01-25 00:12:28 +0000

    updated discord message

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,14 +7,9 @@ class TestSendDiscordMessage(unittest.TestCase):
     @patch('requests.post')
     def test_send_discord_message_success(self, mock_post):
         mock_post.return_value.status_code = 204
-        send_discord_message('https://discord.com/api/webhooks/1332483720296534087/jPo2eLAFw4uASEvlyEMPDlbwYOjys_Ffvq-5ueWDoM2AOOVjOsxqRCqtzNxqw_xmkFUa', 'Hello, Discord!')
+        send_discord_message('https://discord.com/api/webhooks/1332483720296534087/jPo2eLAFw4uASEvlyEMPDlbwYOjys_Ffvq-5ueWDoM2AOOVjOsxqRCqtzNxqw_xmkFUa', 'This is from github actions!🤣🤣🤣🤣')
         self.assertIn('Message sent successfully', 'Message sent successfully')
 
-    @patch('requests.post')
-    def test_send_discord_message_failure(self, mock_post):
-        mock_post.return_value.status_code = 400
-        send_discord_message('http://example.com/webhook', 'Hello, Discord!')
-        self.assertIn('Failed to send message: 400', 'Failed to send message: 400')
 
 if __name__ == '__main__':
     unittest.main()
